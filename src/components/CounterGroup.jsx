da5ec675cb768c431711bd7d5c60d025d3dc3a6f
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-27 23:47:50 +0800

    [React-Day1] feat: Update CounterGroup component to include PropTypes validation

--- src/components/CounterGroup.jsx
+++ src/components/CounterGroup.jsx
@@ -1,12 +1,31 @@
 import Counter from "./Counter";
-const CounterGroup = ({ size}) => {
+import PropTypes from "prop-types";
+
+const CounterGroup = ({ sizeArray, Counts, setCounts }) => {
+    const updateCounts = (index, value) => {
+        const newCounts = [...Counts];
+        newCounts[index] = value;
+        setCounts(newCounts);
+    };
+
     return (
         <div>
-            {Array.from({ length: size }).map((_, index) => (
-                <Counter key={index} />
+            {sizeArray.map((_, index) => (
+                <Counter
+                    key={`counter-${index}`}
+                    index={index}
+                    count={Counts[index]}
+                    setCount={updateCounts}
+                />
             ))}
         </div>
     );
+};
+
+CounterGroup.propTypes = {
+    sizeArray: PropTypes.arrayOf(PropTypes.number).isRequired,
+    Counts: PropTypes.arrayOf(PropTypes.number).isRequired,
+    setCounts: PropTypes.func.isRequired,
 };
 
 export default CounterGroup;
